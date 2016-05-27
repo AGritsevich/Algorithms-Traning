a86221ffc948d842bfd5afab6f847ae715ad91ff
Kozoriz <Kozoriz@github.com>
2016-05-27 11:38:33 +0300

    nary search 2

--- Algorithm_traning_vs2015/Algorithm_traning_vs2015/Algorithm_traning_vs2015.cpp
+++ Algorithm_traning_vs2015/Algorithm_traning_vs2015/Algorithm_traning_vs2015.cpp
@@ -13,9 +13,9 @@ void test(T expected, TFunc f, P1 p1, P2 p2)
 	auto result = f(p1, p2);
 	if (expected != result)
 	{
-		std::cout << "Error: "
+		std::cout << "testing: " << expected
 			<< "\texpected: " << expected
-			<< "\ngot: " << result << endl;
+			<< "== f(" << p1 << ", " << p2 << "), but  ==" << result << endl;
 
 	}
 	else
@@ -23,6 +23,27 @@ void test(T expected, TFunc f, P1 p1, P2 p2)
 		cout << "Test ok " << endl;
 	}
 
+	if (expected != result)
+	{
+		std::cout << "Error: "
+			<< "\texpected: " << expected
+			<< "!= f(" << p1 << ", " << p2 << "), but " << result << endl;
+
+	}
+	else
+	{
+		cout << "Test ok " << endl;
+	}
+
+}
+
+ostream& operator<<(ostream& o, const vector<int> &v)
+{
+	o << "{";
+	for (const auto& x : v)
+		o << x << ",";
+	o << "}";
+	return o;
 }
 
 template <class TFunc>
@@ -46,7 +67,7 @@ void test_binary_search(TFunc binary_search_impl)
 	// second trivial
 	test(-1, adaptor, Vec({ 1, 2 }), key);
 	test(1, adaptor, Vec({ 1, key }), key);
-	test(-1, adaptor, Vec({ key, 100 }), key);
+	test(0, adaptor, Vec({ key, 100 }), key);
 	
 	// key not in array
 	test(-1, adaptor, Vec({ 1, 2, 3, 5, 41 }), key);
@@ -54,14 +75,21 @@ void test_binary_search(TFunc binary_search_impl)
 	test(-1, adaptor, Vec({ 3, 5, 41, 43, 45,67 }), key);
 	
 	// key in array 
-	test(3, adaptor, Vec({ 1, 2, 3, 5, key }), key);
+	test(4, adaptor, Vec({ 1, 2, 3, 5, key }), key);
 	test(0, adaptor, Vec({ key, 45, 67, 100 }), key);
 	test(3, adaptor, Vec({ 3, 5, 41, key, 45,67 }), key);
 
 	// binary search specific
 	test(3, adaptor, Vec({ 3, 5, 41, key, 45,67 }), key);
-	test(3, adaptor, Vec({ 2, 5, key, 45,67 }), key);
+	test(2, adaptor, Vec({ 2, 5, key, 45,67 }), key);
 
+	// more than one key
+	test(1, adaptor, Vec({key, key}), key);
+	test(3, adaptor, Vec({ 1, 2, key, key }), key);
+	test(4, adaptor, Vec({ 1, 2, 3, key, key }), key);
+	test(1, adaptor, Vec({ key, key, key+1, key+10 }), key);
+
+	
 }
 //////////////////////////////////////////////////////////////////////////////
 
@@ -89,6 +117,35 @@ TIter binary_search_1(TIter begin, TIter end, T key)
 	else
 	{
 		return binary_search_1(m, end, key);
+	}
+}
+
+template <class TIter, class T>
+TIter binary_search_2(TIter begin, TIter end, T key)
+{
+	assert(std::is_sorted(begin, end));
+	auto size = end - begin;
+	if (size == 0)
+	{
+		return end;
+	}
+
+	assert(size > 1);
+
+	auto m = begin + (end - begin) / 2;
+
+	if (key < *m)
+	{
+		auto r = binary_search_2(begin, m, key);
+		return m == r ? end : r;
+	}
+	else if (*m < key) // [m+1, end)
+	{
+		return binary_search_2(m, end, key);
+	}
+	else
+	{
+		return *m;
 	}
 }
 //////////////////////////////////////////////////////////////////////////////
